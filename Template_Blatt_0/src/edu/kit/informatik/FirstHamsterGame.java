f0c0bf4716b24893ec1f8ff8e23ca7259b152f33
PhilippHorn04 <146627718+PhilippHorn04@users.noreply.github.com>
2023-11-09 23:35:11 +0100

    Add files via upload

---
+++ Template_Blatt_0/src/edu/kit/informatik/FirstHamsterGame.java
@@ -0,0 +1,154 @@
+package edu.kit.informatik;
+
+import de.hamstersimulator.objectsfirst.external.model.Hamster;
+import de.hamstersimulator.objectsfirst.inspector.InspectableSimpleHamsterGame;
+
+/**
+ * A simple HamsterGame where paule can walk a path of grains
+ */
+public class FirstHamsterGame extends InspectableSimpleHamsterGame {
+
+    /**
+     * This is an instance of the paule from the class Hamster
+     */
+    protected final Hamster paule;
+
+    public FirstHamsterGame(Hamster paule) {
+        this.paule = paule;
+    }
+
+    /**
+     * Creates a new FirstHamsterGame
+     */
+    public FirstHamsterGame() {
+        this.loadTerritoryFromResourceFile("/territories/territory.ter");
+        this.displayInNewGameWindow();
+        this.game.startGame();
+        this.paule = this.game.getTerritory().getDefaultHamster();
+    }
+
+    /*
+     * The main method indicates the compiler the entry point to the program.
+     * First, a new instance (game) of the class FirstHamsterGame is created.
+     * Then, with the operation game.run() the method run() of the class FirstHamsterGame is executed.
+     */
+    public static void main(String[] args) {
+        final FirstHamsterGame game = new FirstHamsterGame();
+        game.doRun();
+    }
+
+    /**
+     * Executed after the game is started.
+     * Can be used to execute hamster commands.
+     */
+    @Override
+    public void run() {
+        gotostart();
+    	paule.turnLeft();
+    	paule.turnLeft();
+
+    
+    	while (paule.frontIsClear()) {
+            if (paule.grainAvailable()) {
+                paule.pickGrain();
+            }
+            paule.move();
+        }
+        turnEdgeontheright();
+        while (paule.frontIsClear()) {
+            if (paule.grainAvailable()) {
+                paule.pickGrain();
+            }
+            paule.move();
+        }
+        turnEdgeontheleft();
+        
+        while (paule.frontIsClear()) {
+            if (paule.grainAvailable()) {
+                paule.pickGrain();
+            }
+            paule.move();
+        }
+        turnEdgeontheright();
+        while (paule.frontIsClear()) {
+            if (paule.grainAvailable()) {
+                paule.pickGrain();
+            }
+            paule.move();
+        }
+        paule.turnLeft();
+    	paule.turnLeft();
+    	paule.turnLeft();
+        paule.move();
+        paule.move();
+        paule.turnLeft();
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.move();
+    	paule.pickGrain();
+    	paule.pickGrain();
+    	gohome();
+        
+        
+    }
+
+    /**
+     * Method to make Paule turn around (180 degrees)
+     */
+    private void gotostart() {
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.move();
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.move();
+    	
+    }
+    private void turnEdgeontheright() {
+    	paule.turnLeft();
+        paule.move();
+        paule.turnLeft();
+    }
+    private void turnEdgeontheleft() {
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.turnLeft();
+        paule.move();
+        paule.turnLeft();
+        paule.turnLeft();
+    	paule.turnLeft();
+    }
+    private void gohome() {
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.move();
+    	paule.turnLeft();
+    	paule.move();
+    	paule.move();
+    	paule.move();
+    	paule.turnLeft();
+    	paule.move();
+    	paule.move();
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.turnLeft();
+    	paule.move();
+    	paule.move();
+    	while(paule.mouthEmpty() == false) {
+    		paule.putGrain();
+    	}
+    	
+    }
+        
+
+    /*
+     * Exercise for students with prior knowledge in programming:
+     * Implement the method turnRight, that lets Paule turn right.
+     */
+    private void turnRight() {
+        //
+    }
+}
